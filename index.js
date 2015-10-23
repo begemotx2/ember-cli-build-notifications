3a83664b788f6f72b62b6d96798562d827667cd2
Ivan Terpugov <ivan@diplomero.com>
2015-10-23 12:33:32 +0300

    added postBuild notification

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ var Config = require('./lib/config');
 module.exports = {
   name: 'ember-cli-build-notifications',
 
+  postBuild: function(result){
+    var seconds = result.totalTime / 1000000000;
+    notifier.notify({
+      title: 'Build success',
+      message: 'Build time: ' + (Math.round(seconds * 100) / 100) + ' seconds',
+      appIcon: path.resolve(__dirname + '/ember-logo.png'),
+      'expire-time': 1000
+    });
+  },
+
   buildError: function(error) {
     var config = Config.load(this.project.root);
 
